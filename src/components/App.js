06b92a92986a8e5d83264bcfaa16c99e5671d543
imhsart <nitinrana2519@gmail.com>
2026-06-05 19:12:37 +0530

    Assignment attempt 8

--- src/components/App.js
+++ src/components/App.js
@@ -7,44 +7,37 @@ const productsArr = ['Samsung Galaxy Fold 4', 'Iphone 14 Pro','Pixel 5', 'Mi not
 const App = () => {
   const [cartProducts, setCartProducts] = useState([])
   const [count, setCount] = useState(0)
-
-  function handleClick(val) {
-    setCartProducts(prev => [...prev, { id: count, product: val }])
-    setCount(prev => prev + 1)
+  function handleClick(val){
+    setCartProducts(prev => [...prev, {id: count, product: val}])
+    setCount(prev => prev+1)
   }
-
-  function handleDelete(id) {
-    setCartProducts(cartProducts.filter(val => val.id !== id))
+  function handleDelete(idx){
+    setCartProducts(cartProducts.filter(val => val.id !== idx))
   }
 
   return (
     <div>
-      <h1>List of Products</h1>
-      <div className="container">
-        {productsArr.map((item, idx) => (
-          <div className="item" key={idx}>
-            <p>{item}</p>
-            <button onClick={() => handleClick(item)}>Add Item</button>
-          </div>
-        ))}
-      </div>
-      <hr />
-      <div className="cart">
-        <h1>Cart</h1>
-        {!cartProducts.length
-          ? <p style={{ color: 'red', margin: '20px' }}>There are no items in the cart</p>
-          : <ul>
-              {cartProducts.map((item, idx) => (
-                <li className="cart-item" key={item.id}>
-                  {item.product}
-                  <button onClick={() => handleDelete(item.id)}>
-                    {idx + 1}
-                  </button>
-                </li>
-              ))}
-            </ul>
-        }
-      </div>
+        <h1>List of Products</h1>
+        <div className="container">
+          {
+            productsArr.map(item => <div className="item">
+                <p className="list-item">{item}<button className="add-btn" onClick={() => handleClick(item)}>Add Item</button></p>
+              </div>)
+          }
+        </div>
+        <hr />
+        <div className="cart">
+          <h1>Cart</h1>
+          {!cartProducts.length ? <p style={{color:'red', margin:'20px'}}>There are no items in the cart</p> :
+          <ul>
+            {
+              cartProducts.map(item => {
+                return <li className="cart-item" key={item.id}>{item.product}<button onClick={() => handleDelete(item.id)}>Remove</button></li>
+              })
+
+            }
+          </ul>}
+        </div>
     </div>
   )
 }
