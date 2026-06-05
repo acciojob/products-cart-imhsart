b8fb5021d91205ea4d0500162d37d9975aa6a111
imhsart <nitinrana2519@gmail.com>
2026-06-05 20:18:21 +0530

    Assignment attempt 12

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ const App = () => {
         <h1>List of Products</h1>
         <div className="container">
           {
-            productsArr.map(item => <p className="item">{item}<button className="add-btn" onClick={() => handleClick(item)}>Add Item</button></p>)
+            productsArr.map(item => <div className="item">{item}<button className="add-btn" onClick={() => handleClick(item)}>Add Item</button></div>)
           }
         </div>
         <hr />
